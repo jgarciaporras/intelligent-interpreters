e4f85f92e567d2230b728adc605fd0b2e167842f
Saptarshi Kundu <me@saptarshikundu.com>
2022-02-22 19:49:08 -0500

    added speech_to_text function

--- modules/speech_to_text.py
+++ modules/speech_to_text.py
@@ -1,4 +1,6 @@
 import pydub
+import os
+from google.cloud import speech
 
 
 def convert_wav_to_mp3(audio_clip_filename, destination_path):
@@ -9,3 +11,31 @@ def convert_wav_to_mp3(audio_clip_filename, destination_path):
     sound.export(mp3_fillename, format="mp3")
     print("File Converted Successfully")
     return
+
+
+def speech_to_text(mp3_audio_file_name, client_service_key_location):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = client_service_key_location
+    speech_client = speech.SpeechClient()
+    media_file_name = 'audio_clips/converted/' + mp3_audio_file_name + '.mp3'
+    
+    with open (media_file_name, 'rb') as f:
+        byte_data_wav = f.read()
+    
+    audio_mp3 = speech.RecognitionAudio(content=byte_data_wav)
+    
+    config_mp3 = speech.RecognitionConfig(
+        sample_rate_hertz=48000,
+        enable_automatic_punctuation=True,
+        language_code='en-US'
+    )
+
+    # Transcribing the RecognitionAudio objects
+    response_standard_mp3 = speech_client.recognize(
+        config=config_mp3,
+        audio=audio_mp3
+    )
+
+    transcription = str()
+    for result in response_standard_mp3.results:
+        transcription.append(result.alternatives[0].transcript)
+
